0a27662a4a169cd5302d0ddab9f2fc610a0618ff
Sara <saramalagamba@gmail.com>
2023-10-24 20:52:10 +0200

    Ejercicio 3

--- src/js/exercises/exercise-3.js
+++ src/js/exercises/exercise-3.js
@@ -3,3 +3,37 @@ EJERCICIO 3:
 Muestra el siguiente mensaje con el resultado de contar el número de vocales que tiene un string:
 “Number of vowels is <result>”
 */
+
+function counterVowels(str) {
+
+    let transfLowerCase = str.toLowerCase();
+
+    let result = 0;
+
+    for (let i = 0; i < transfLowerCase.length; i++) {
+
+        let vowel = transfLowerCase.charAt(i);
+
+        switch (vowel){
+
+            case "a": ++result;
+                break;
+            case "e": ++result;
+                break;
+            case "i": ++result;
+                break;
+            case "o": ++result;
+                break;
+            case "u": ++result;
+                break;
+            default: console.log("no es una vocal");
+
+        }
+
+    }
+
+    return `Number of vowels is ${result}`;
+
+}
+
+showContent(3, counterVowels("EI94aadPEIDMHDUWEHu"));
