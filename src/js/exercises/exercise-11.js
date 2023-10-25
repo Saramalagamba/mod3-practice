01d194e7bf94dfa7b5e8389618bfe5eef67a2198
Sara <saramalagamba@gmail.com>
2023-10-25 17:19:25 +0200

    Ejercicio 11

--- src/js/exercises/exercise-11.js
+++ src/js/exercises/exercise-11.js
@@ -2,3 +2,8 @@
 EJERCICIO 11:
 Ordena las peliculas por valoracion (propiedad "rating") de mayor a menor
 */
+  
+  const arrayOfRating = movies.map((movie) => movie.rating);
+  const sortedArrayOfRating = arrayOfRating.sort();
+  
+  showContent(11, sortedArrayOfRating);
