4391b3407da07acc45dfd229601e34de4aab392f
Sara <saramalagamba@gmail.com>
2023-10-29 17:40:59 +0100

    Ejercicio 20

--- src/js/exercises/exercise-20.js
+++ src/js/exercises/exercise-20.js
@@ -12,3 +12,28 @@ Calcula el Fib de un número pasado como parámetro y muestra un mensaje:
 
 Nota: evita calcular números altos.
 */
+
+function calculateFibonacci(serieFib) {
+    switch (serieFib) {
+        case 0: return (`El 0 número de la serie de Fibonacci es 0`);
+            break;
+        case 1: return (`El primer número de la serie de Fibonacci es 1`);
+            break;
+        case 2: return (`El segundo número de la serie de Fibonacci es 1`);
+            break;
+        case 3: return (`El tercer número de la serie de Fibonacci es 2`);
+            break;
+        default:
+            let arrayOfFibo = [0, 1, 1, 2];
+            let result;
+            for (i = 4; i <= serieFib; i++) {
+                result = 0;
+                result = arrayOfFibo[i - 1] + arrayOfFibo[i - 2];
+                console.log(result);
+                arrayOfFibo.push(result);
+                console.log(arrayOfFibo);
+            }
+            return (`El ${serieFib}º número de la serie de Fibonacci es ${result}`);
+    }
+}
+showContent(20, calculateFibonacci(9));
