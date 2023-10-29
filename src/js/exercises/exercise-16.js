a48a0b379f34987cc0aecb6494ca1f2d95604807
Sara <saramalagamba@gmail.com>
2023-10-29 13:08:47 +0100

    Ejercicio 16

--- src/js/exercises/exercise-16.js
+++ src/js/exercises/exercise-16.js
@@ -2,3 +2,9 @@
 EJERCICIO 16:
 Dado un año como parámetro (número), devuelve el número del siglo al que pertenece.
 */
+
+function calculateCentury (year){
+    let century = Math.floor((year / 100)) + 1;
+    return (`El año ${year} corresponde al siglo ${century}`)
+}
+showContent(16, calculateCentury(1975));
